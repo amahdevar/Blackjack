1d175da150448ebeacf551a2ec5a2ec0fc383608
amahdevar <abolfazlmahdevar@gmail.com>
2022-08-10 13:06:51 +0430

    Reworked startGame() and added player object and added stop game after player loses

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-// This is Meysam test file.
-
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
+// Blackjack Game on Html.
+let player = { // Player object.
+    name: "Meysam",
+    chips: 12
+}
 let hasBlackJack = false;
-let isAlive = true;
-let message = ""
-sum = firstCard + secondCard;
-let cards = [firstCard,secondCard];
+let isAlive = false;
+let message = "";
+let cards = [];
+let sum = 0;
 let messageEl = document.getElementById("message-el");
 let sumEl = document.querySelector(".sum-el");
 let cardsEl = document.querySelector("#cards-el");
+let playerEl = document.getElementById("player-el");
+playerEl.textContent = player.name + ": $" + player.chips
+
 function getRandomCard(){
     let randomNumber = Math.floor(Math.random() * 10) + 2
     return randomNumber
@@ -35,11 +39,18 @@ function renderGame() {
 
 }
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    sum = firstCard + secondCard
+    cards = [firstCard,secondCard];
     renderGame();
 }
 function newCard() {
+    if (isAlive === true && hasBlackJack === false ) {
     let card = getRandomCard();
     sum += card;
     cards.push(card)
     renderGame();
+}
 }
